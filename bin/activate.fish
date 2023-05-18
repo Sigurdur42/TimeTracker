ef9b15448a1116398e95f9b0aab48f79e5f415f7
Michael Wagner <MichaelWagner42@googlemail.com>
2023-05-18 09:17:34 +0200

    .

--- bin/activate.fish
+++ bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mwagner/dev/TimeTracker"
+set -gx VIRTUAL_ENV "/home/michael/dev/TimeTracker"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
