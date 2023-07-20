961a4b9f0ffd42a145e158148ee184bfeae3817c
Michael Wagner <MichaelWagner42@googlemail.com>
2023-07-20 08:07:02 +0200

    Added proper ico

--- createPackage.bat
+++ createPackage.bat
@@ -1,8 +1,11 @@
-pyinstaller main.py -n "TimeTracker" --noconfirm --add-binary "src/MainWindow.ui;src/." --add-binary "src/EditRecord.ui;src/."
+pip install -r requirements.txt
+pyinstaller main.py -n "TimeTracker" --noconfirm --add-binary "src/MainWindow.ui;src/." --add-binary "src/EditRecord.ui;src/." -i "src/clock.ico"
 If exist ".\dist\src" ( 
     Echo "dist\src already exists"
 ) Else ( 
     Echo "creating ui folder"
     mkdir ".\dist\src"
 )
-copy .\src\*.ui .\dist\src
+copy .\src\*.ui .\dist\TimeTracker\src
+copy .\src\*.ico .\dist\TimeTracker\src
+copy .\src\*.png .\dist\TimeTracker\src
--- createPackage.sh
+++ createPackage.sh
@@ -1,3 +1,6 @@
+pip install -r requirements.txt
 pyinstaller main.py --onefile --noconfirm -n "TimeTracker"
 mkdir -p ./dist/src/
 cp ./src/*.ui ./dist/src/
+cp ./src/*.ico ./dist/src/
+cp ./src/*.png ./dist/src/
