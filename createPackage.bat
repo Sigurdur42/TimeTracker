592f3a9f0763a3084bf943cd0bef8c6b4aa4987e
Michael Wagner <MichaelWagner42@googlemail.com>
2023-11-09 15:15:20 +0100

    Fixed batch

--- createPackage.bat
+++ createPackage.bat
@@ -1,10 +1,10 @@
 pip install -r requirements.txt
 pyinstaller .\TimeTracker.spec --noconfirm
-If exist ".\dist\src" ( 
+If exist ".\dist\TimeTracker\src" ( 
     Echo "dist\src already exists"
 ) Else ( 
     Echo "creating ui folder"
-    mkdir ".\dist\src"
+    mkdir ".\dist\TimeTracker\src"
 )
 copy .\src\*.ui .\dist\TimeTracker\src
 copy .\src\*.ico .\dist\TimeTracker\src
